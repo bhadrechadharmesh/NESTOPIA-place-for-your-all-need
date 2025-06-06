5cebd93baae115dba8ed325fe5d84ceca53a9c65
bhadrechadharmesh <bhadrechadharmesh141@gmail.com>
2025-06-06 20:47:48 +0530

    add ejs-mate template

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const methodOverride= require("method-override");
+const ejsMate = require("ejs-mate");
 
 const app = express();
 const port = 3000;
@@ -13,6 +14,8 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+app.engine('ejs',ejsMate);
+app.use(express.static(path.join(__dirname,"/public")));
 
 main()
   .then(() => {
